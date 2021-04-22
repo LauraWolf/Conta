210abfaaf54ec5b74c0c1bee37503821aaabab09
LauraWolf <wolflaura03@gmail.com>
2021-04-22 11:52:29 -0300

    Decimo commit

---
+++ src/sample/MainCliente.java
@@ -0,0 +1,17 @@
+package sample;
+import sample.model.Cliente;
+import sample.model.Conta;
+
+public class MainCliente {
+    public static void main(String[] args) {
+        Conta conta = new Conta(1234, 4566);
+        conta.depositar(3000);
+
+        Cliente cliente = new Cliente();
+        cliente.setConta(conta);
+        cliente.setNome("Laura");
+        cliente.setCpf("432.112.222-09");
+
+        System.out.println(cliente);
+    }
+}
